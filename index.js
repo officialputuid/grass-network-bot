0471030bd82c1b170748cd1f99297aa95e0d5d78
recitativonika <fool-body-cheddar@duck.com>
2024-11-07 14:31:59 +0700

    Add auto restart when websocket error/dropped

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ const WebSocket = require('ws');
 const { v4: uuidv4 } = require('uuid');
 require('colors');
 
+const WEBSOCKET_URL = 'wss://proxy.wynd.network:4444';
+const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36';
+const PING_INTERVAL = 30000;
+
 function fetchPublicIP(callback) {
   https.get('https://api.ipify.org?format=json', (res) => {
     let data = '';
-
-    res.on('data', (chunk) => {
-      data += chunk;
-    });
-
+    res.on('data', (chunk) => { data += chunk; });
     res.on('end', () => {
       const ipObject = JSON.parse(data);
       callback(ipObject.ip);
     });
   }).on('error', (err) => {
     console.error(`Error fetching public IP: ${err.message}`.red);
+	startProcess();
   });
 }
 
 function centerText(text) {
   const width = process.stdout.columns || 80;
-  const paddedText = text.padStart((width + text.length) / 2);
-  return paddedText;
+  return text.padStart((width + text.length) / 2);
 }
 
 function displayIntro() {
@@ -38,12 +38,10 @@ function displayIntro() {
 async function initiateWebSocketConnection(userID) {
   fetchPublicIP((publicIP) => {
     console.log(centerText(`Your Public IP Address: ${publicIP}`).green);
-
     try {
-      const websocketURL = `wss://proxy.wynd.network:4444`;
-      const websocket = new WebSocket(websocketURL, {
+      const websocket = new WebSocket(WEBSOCKET_URL, {
         headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
+          'User-Agent': USER_AGENT,
           pragma: 'no-cache',
           Origin: 'chrome-extension://lkbnfiajjmbhnfledhphioinpickokdi',
           'Accept-Language': 'uk-UA,uk;q=0.9,en-US;q=0.8,en;q=0.7',
@@ -61,7 +59,7 @@ async function initiateWebSocketConnection(userID) {
         
         if (parsedMessage.action === 'AUTH') {
           console.log(centerText(`AuthID: ${parsedMessage.id}`).cyan);
-		  console.log('\n');
+          console.log('\n');
           sendAuthResponse(websocket, parsedMessage.id, userID);
         } else if (parsedMessage.action === 'PONG') {
           console.log(`Received PONG: ${JSON.stringify(parsedMessage)}`.blue);
@@ -73,13 +71,19 @@ async function initiateWebSocketConnection(userID) {
         if (reason === "nggyu") {
           console.error(centerText(`Might be because too many auth attempts. Try again later.`).red);
         }
+        console.log(`Restarting the connection...`.cyan);
+        startProcess();
       });
 
       websocket.on('error', (error) => {
         console.error(`WebSocket error: ${error.message}`.red);
+        console.log(`Restarting the connection due to WebSocket error...`.cyan);
+        startProcess();
       });
     } catch (error) {
       console.error(`Error connecting to WebSocket: ${error.message}`.red);
+      console.log(`Restarting the connection due to connection error...`.cyan);
+      startProcess();
     }
   });
 }
@@ -91,16 +95,16 @@ function sendAuthResponse(websocket, messageId, userID) {
     result: {
       browser_id: uuidv4(),
       user_id: userID,
-      user_agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
+      user_agent: USER_AGENT,
       timestamp: Math.floor(Date.now() / 1000),
       device_type: 'extension',
       extension_id: 'lkbnfiajjmbhnfledhphioinpickokdi',
       version: '4.26.2',
     },
   };
-  
+
   websocket.send(JSON.stringify(authResponse));
-  console.log(`Trying to authenticating and send PING to server, it might take a sec`.cyan);
+  console.log(`Attempting to authenticate and send PING to server. It might take a moment...`.cyan);
 }
 
 function startPing(websocket, ip) {
@@ -111,20 +115,23 @@ function startPing(websocket, ip) {
       ip: ip,
       timestamp: timestamp,
     };
-    
+
     websocket.send(JSON.stringify(pingData));
-    
     console.log(`Sending PING from IP: ${ip.yellow}, Timestamp: ${timestamp.toString().green}`);
-  }, 30000);
+  }, PING_INTERVAL);
 }
 
-fs.readFile('userid.txt', 'utf8', (error, data) => {
-  if (error) {
-    console.error(`Error reading userID from file: ${error.message}`.red);
-    return;
-  }
+function startProcess() {
+  fs.readFile('userid.txt', 'utf8', (error, data) => {
+    if (error) {
+      console.error(`Error reading userID from file: ${error.message}`.red);
+      return;
+    }
 
-  const userID = data.trim();
-  displayIntro();
-  initiateWebSocketConnection(userID);
-});
+    const userID = data.trim();
+    displayIntro();
+    initiateWebSocketConnection(userID);
+  });
+}
+
+startProcess();
